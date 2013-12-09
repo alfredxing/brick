7b845201851f346a5a69f8d1705d0e812cc602c9
Alfred Xing <xing@lfred.info>
2013-12-09 14:39:31 -0800

    Add index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+header('Content-type: text/css');
+header('X-Content-Type-Options: nosniff');
+header('Cache-Control: public, max-age=86400');
+header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + 86400));
+header('Pragma: Public');
+
+$TEMPLATE = "@font-face{font-family:'%s';font-style:%s;font-weight:%s;src:local('%s'),url(%s) format('opentype'),url(%s) format('woff')}";
+
+$query = explode("/", preg_replace("/\/$|^\//", "", urldecode($_SERVER['REQUEST_URI'])));
+
+/*$memcache = new Memcache;
+$cat = $memcache->get('cat');
+if ($cat === false) {
+	$cat = json_decode(file_get_contents('./cat.json'));
+	$memcache->set('cat', $cat);
+}*/
+$cat = json_decode(file_get_contents('./cat.json'));
+
+foreach ($query as $key=>$val) {
+	$val = explode(":", $val);
+	$family = preg_replace("/\+/", " ", $val[0]);
+	$weights = explode(",", $val[1]);
+
+	foreach ($weights as $weight) {
+		$base_url = 'https://raw.github.com/alfredxing/brick/master/' . strtolower(preg_replace("/\s/", '', $family)) . "/";
+		$local = $cat->$family->$weight;
+		$otf = $base_url . $weight . ".otf";
+		$woff = $base_url . $weight . ".woff";
+		if (preg_match("/i$/", $weight)) {
+			$style = 'italic';
+			$weight = rtrim($weight, "i");
+		} else {
+			$style = 'normal';
+		}
+		echo sprintf($TEMPLATE, $family, $style, $weight, $local, $otf, $woff);
+	}
+}
+?>
