aa161f83a1ee11d219db7ee4d7e6c6a8ca2a857c
Alfred Xing <xing@lfred.info>
2013-12-22 14:01:29 -0800

    Add flags
    Flag  forces downloading of font, bypassing any locally installed ones

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ header('Cache-Control: public, max-age=86400');
 header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + 86400));
 header('Pragma: Public');
 
+// Templates
 $TEMPLATE = "@font-face{font-family:'%s';font-style:%s;font-weight:%s;src:local('%s'),url(%s) format('svg'),url(%s) format('opentype'),url(%s) format('woff')}";
+$TEMPLATE_FORCE = "@font-face{font-family:'%s';font-style:%s;font-weight:%s;src:url(%s) format('svg'),url(%s) format('opentype'),url(%s) format('woff')}";
 
 $query = explode("/", preg_replace("/\/$|^\//", "", urldecode($_SERVER['REQUEST_URI'])));
 $cat = json_decode(file_get_contents('./cat.json'));
@@ -14,6 +16,7 @@ foreach ($query as $key=>$val) {
 	$val = explode(":", $val);
 	$family = preg_replace("/\+/", " ", $val[0]);
 	$weights = explode(",", $val[1]);
+	$flags = isset($val[2]) ? $val[2] : '';
 
 	foreach ($weights as $weight) {
 		$base_url = (empty($_SERVER['HTTPS']) ? 'http:' : 'https:') . '//get.brick.im/' . strtolower(preg_replace("/\s/", '', $family)) . "/";
@@ -30,7 +33,12 @@ foreach ($query as $key=>$val) {
 		} else {
 			$style = 'normal';
 		}
-		echo sprintf($TEMPLATE, $family, $style, $weight, $local, $svg, $otf, $woff);
+
+		// Process flags
+		if (strpos($flags,'f') !== false)
+			echo sprintf($TEMPLATE, $family, $style, $weight, $local, $svg, $otf, $woff);
+		else
+			echo sprintf($TEMPLATE, $family, $style, $weight, $local, $svg, $otf, $woff);
 	}
 }
 ?>
