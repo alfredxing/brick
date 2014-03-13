8f98232f285257a5030076b6f1db08384d7a548d
Alfred Xing <alfred@xng.pw>
2014-03-13 15:11:27 -0700

    Remove GeoIP stuff

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-// GeoIP processing resources
-require_once 'vendor/autoload.php';
-use MaxMind\Db\Reader;
-
 header('Content-type: text/css');
 header('X-Content-Type-Options: nosniff');
 header('Cache-Control: public, max-age=86400');
@@ -29,17 +25,7 @@ $SERVERS = array(
 	"//sea.cdn.brick.im/" => array( 47.7542, -122.2444 )
 	);
 
-// Latitude/longitude distance function
-function distance($a,$b,$c,$d){$e=$b-$d;$f=sin(deg2rad($a))*sin(deg2rad($c))+cos(deg2rad($a))*cos(deg2rad($c))*cos(deg2rad($e));$f=acos($f);$f=rad2deg($f);return $f;}
-
-$reader = new Reader('vendor/GeoLite2-City.mmdb');
-$user = $reader->get($_SERVER['REMOTE_ADDR'])["location"];
-$result = array();
-foreach ($SERVERS as $host => $loc) {
-    $result[$host] = distance($user["latitude"], $user["longitude"], $loc[0], $loc[1]);
-}
-// Finally, determine closest server
-$server = array_keys($result, min($result))[0];
+$server = $SERVERS[0];
 
 foreach ($query as $key=>$val) {
 	$val = explode(":", $val);
--- vendor/maxmind-db/reader/tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
-    die('Project dependencies missing');
-}
-
-$loader->add('MaxMind\Db\Test', __DIR__);
